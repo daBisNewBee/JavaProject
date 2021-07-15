b7099212abdaf2b271a4360e2d8c7554aec09b16
wenbin.liu <wenbin.liu@ximalaya.com>
2021-07-16 01:08:39 +0800

    "两数之和S": 输入数组无序：用map，空间换时间，原来o(n2)，现在o(n) 输入数组有序：双指针

--- src/offer/other/42_findNumbersWithSum.kt
+++ src/offer/other/42_findNumbersWithSum.kt
@@ -38,6 +38,36 @@ fun findNumbersWithSum(array: IntArray, sum:Int):MutableList<Int>{
     return result
 }
 
+/**
+ *
+ * "两数之和S"
+ *
+ *给定一个整数数组 nums 和一个整数目标值 target，
+ * 请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+
+    你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+
+ 注意和上题的区别：
+ 1. 数组无序
+ 2. 只有一组解
+
+ 用map 空间换时间，否则要遍历，时间o(n2)
+ 时间、空间都是o(n)
+ *
+ */
+fun twoSum(nums: IntArray, target: Int): IntArray {
+    if(nums.isEmpty()) return IntArray(0)
+    var map = HashMap<Int, Int>(2)
+    for(i in nums.indices) {
+        if(map.containsKey((target-nums[i]))){
+            return intArrayOf(map[target-nums[i]]!!, i)
+        } else {
+            map[nums[i]] = i
+        }
+    }
+    return IntArray(0)
+}
+
 fun main(){
     var data = intArrayOf(1,2,3,4,5,6,7,8,9,10)
     println(findNumbersWithSum(data,10))
