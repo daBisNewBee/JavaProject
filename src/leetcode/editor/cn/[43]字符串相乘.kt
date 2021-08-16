05927aa51bf13c399f2721db466b67d88ecc8fa9
wenbin.liu <wenbin.liu@ximalaya.com>
2021-08-16 22:41:55 +0800

    [43]字符串相乘

---
+++ src/leetcode/editor/cn/[43]字符串相乘.kt
@@ -0,0 +1,66 @@
+//给定两个以字符串形式表示的非负整数 num1 和 num2，返回 num1 和 num2 的乘积，它们的乘积也表示为字符串形式。 
+//
+// 示例 1: 
+//
+// 输入: num1 = "2", num2 = "3"
+//输出: "6" 
+//
+// 示例 2: 
+//
+// 输入: num1 = "123", num2 = "456"
+//输出: "56088" 
+//
+// 说明： 
+//
+// 
+// num1 和 num2 的长度小于110。 
+// num1 和 num2 只包含数字 0-9。 
+// num1 和 num2 均不以零开头，除非是数字 0 本身。 
+// 不能使用任何标准库的大数类型（比如 BigInteger）或直接将输入转换为整数来处理。 
+// 
+// Related Topics 数学 字符串 模拟 
+// 👍 693 👎 0
+
+
+class P_43_MultiplyStrings {
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+        /**
+         *
+         * 优化竖式法：
+         *
+         * 几个规律点：
+         * 1. num1(m位) * num2(n位) = res
+         * res 的长度为 m + n 位
+         *
+         * 2. num1的第i位 * num2的第j位 的结果tmp
+         * 的两位分别位于 res 的res[i+j]、res[i+j+1]
+         *
+         */
+    fun multiply(num1: String, num2: String): String {
+        if (num1 == "0" || num2 == "0") {
+            return "0"
+        }
+        var res = IntArray(num1.length + num2.length)
+        for (i in num1.length - 1 downTo 0) {
+            for (j in num2.length - 1 downTo 0) {
+                var n1 = num1[i] - '0'
+                var n2 = num2[j] - '0'
+                var sum = res[i+j+1] + n1 * n2
+                res[i+j+1] += sum % 10
+                res[i+j] = sum / 10
+            }
+        }
+        var sb = StringBuilder()
+        for (i in res.indices) {
+            if (i == 0 && res[i] == 0) {
+                continue
+            }
+            sb.append(res[i])
+        }
+        return sb.toString()
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
