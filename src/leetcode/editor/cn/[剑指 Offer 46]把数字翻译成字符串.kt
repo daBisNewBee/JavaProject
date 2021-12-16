364e1f94d2ae3351dd0442edc1865571e5569d3e
wenbin.liu <wenbin.liu@ximalaya.com>
2021-12-16 23:42:59 +0800

    [剑指 Offer 46]把数字翻译成字符串：
    动态规划改写暴力尝试

--- src/leetcode/editor/cn/[剑指 Offer 46]把数字翻译成字符串.kt
+++ src/leetcode/editor/cn/[剑指 Offer 46]把数字翻译成字符串.kt
@@ -57,9 +57,8 @@ class Solution {
         }
         if (array[index] == '0') {
             translate(array, index + 1, result, path + "a")
-            return
         }
-        if (array[index] == '1') {
+        else if (array[index] == '1') {
             translate(array, index+1, result, path + "b")
             if (index + 1 < array.size) {
                 var num = String(array, index, 2)
@@ -67,7 +66,7 @@ class Solution {
                 translate(array, index + 2, result, path + c)
             }
         }
-        if (array[index] == '2') {
+        else if (array[index] == '2') {
             translate(array, index+1, result, path + "c")
             if (index + 1 < array.size && array[index+1] >= '0' && array[index+1] <= '5') {
                 var num = String(array, index, 2)
@@ -75,15 +74,85 @@ class Solution {
                 translate(array, index + 2, result, path + c)
             }
         }
+        else {
+            var c = (Integer.valueOf(array[index].toString()) + 'a'.toInt()).toChar()
+            translate(array, index + 1, result, path + c)
+        }
+    }
 
-        var c = (Integer.valueOf(array[index].toString()) + 'a'.toInt()).toChar()
-        translate(array, index + 1, result, path + c)
+        /**
+         *
+         * 动态规划直接改写暴力尝试
+         *
+         * 想想为什么可以直接改写？一些思路！！！
+         *
+         * 1. 一维数组。
+         *    因为只有一个变量:index
+         *
+         * 2. 一维数组长度 array.size + 1。
+         *    因为存在判断"index == array.size"，index最大可以取到 "N"
+         *
+         * 3. 给dp某些元素赋默认值。
+         *    dp[N] = 1
+         *    因为
+         *      if (index == array.size) {
+                    result.add(path)
+                    return
+                }
+              表示当 index == N时，存在一个解
+         *
+         * 3. 遍历方向：从右往左
+         *    因为要求"translate(...0....)" ,即要求dp[0]
+         *    已知dp[N] = 1
+         *    且dp[i] = dp[i+1] + dp[i+2]，当前值依赖更高阶的值
+         *    所以不难得出，方向为从右到左
+         *
+         * 4. 直接复制translate的逻辑，并改写。
+         *    一个"translate(i)"就是一个dp[i] 的求解过程
+         *
+         */
+    fun dpWays(array: CharArray, dp:IntArray):Int {
+        dp[array.size] = 1
+        for (i in array.size-1 downTo 0) {
+            if (array[i] < '0' || array[i] > '9') {
+                continue
+            }
+            when(array[i]) {
+                '0' -> {
+                    dp[i] = dp[i+1]
+                }
+                '1' -> {
+                    dp[i] = dp[i+1]
+                    if (i+1 < array.size) {
+                        dp[i] += dp[i+2]
+                    }
+                }
+                '2' -> {
+                    dp[i] = dp[i+1]
+                    if (i+1 < array.size && array[i+1] >= '0' && array[i+1] <= '5') {
+                        dp[i] += dp[i+2]
+                    }
+                }
+                else -> {
+                    dp[i] = dp[i+1]
+                }
+            }
+        }
+
+        return dp[0]
     }
 
     fun translateNum(num: Int): Int {
         var result = mutableSetOf<String>()
         translate(num.toString().toCharArray(), 0, result, "")
-        return result.size
+
+        var array = num.toString().toCharArray()
+        var dp = IntArray(array.size + 1)
+        var ans = dpWays(num.toString().toCharArray(), dp)
+
+        println("暴力结果：${result.size} 动态规划结果：$ans")
+        return ans
+//        return result.size
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
